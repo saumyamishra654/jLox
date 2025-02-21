45fc19dfd779e0025404bf8579e453649c2bb93c
Saumya Mishra <125398231+saumyamishra654@users.noreply.github.com>
2025-02-21 19:44:53 +0530

    update

--- lox/Environment.java
+++ lox/Environment.java
@@ -34,27 +34,31 @@ class Environment {
     }
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
   }
-
+  // defines a new variable in the current environment
   void define(String name, Object value) { //bind name to value
     values.put(name, value);
   }
 
+    // finds an ancestor environment at a given distance
   Environment ancestor(int distance) {
     Environment environment = this;
     for (int i = 0; i < distance; i++) {
-      environment = environment.enclosing;
+      environment = environment.enclosing; // move up the environment chain
     }
     return environment;
   }
 
+    // retrieves a variable value at a specific distance in the environment chain
   Object getAt(int distance, String name) {
     return ancestor(distance).values.get(name);
   }
 
+  // assigns a new value to an existing variable at a specific distance
   void assignAt(int distance, Token name, Object value) {
     ancestor(distance).values.put(name.lexeme, value);
   }
 
+  // returns a string representation of the environment, including parent environments
   @Override
   public String toString() {
     String result = values.toString();
