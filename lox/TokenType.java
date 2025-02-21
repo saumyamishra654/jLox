5e96455e04b8ffeb2bf91a30dca1fa6a4d078195
Saumya Mishra <125398231+saumyamishra654@users.noreply.github.com>
2025-02-21 16:05:50 +0530

    updating TokenType to include case-switch keywords

--- lox/TokenType.java
+++ lox/TokenType.java
@@ -1,18 +1,27 @@
-package com.craftinginterpreters.lox;
+package lox;
 
 enum TokenType {
-    //single character tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+  //single tokens
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
-    //1-2 character tokens
-    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+  //double character tokens
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
 
-    //literals
-    IDENTIFIER, STRING, NUMBER,
+  //literals
+  IDENTIFIER, STRING, NUMBER,
 
-    //KEYWORDS
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE. VAR, WHILE,
+  //keywords
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
 
-    EOF
+  //end of file
+  EOF,
+
+  //case switch (new addition)
+  SWITCH, CASE, DEFAULT, BREAK
 
 }
